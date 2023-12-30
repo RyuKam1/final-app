ba409ce06c45adb65504ef62802b70e04b313a56
RyuKam1 <omarashvili.giorgi07@gmail.com>
2023-12-31 02:48:30 +0400

    Added contact UI and slight adjustments

--- src/components/postFrame/postFrame.jsx
+++ src/components/postFrame/postFrame.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import Image from "next/image";
 import styles from "./postframe.module.css";
 
-function postFrame({blogPost}) {
+function postFrame({ blogPost }) {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image 
-        src={"/cover.png"}
-        alt="cover"
-        fill = {true}
-        className={styles.img} />
+        <Image
+          src={"/cover.png"}
+          alt="cover"
+          fill={true}
+          className={styles.img}
+        />
       </div>
       <div className={styles.info}>
         <h2 className={styles.title}>{blogPost.title}</h2>
-        <p className={styles.desc}>{blogPost.body}</p>
+        <div className={styles.descContainer}>
+          <p className={styles.desc}>{blogPost.body}</p>
+        </div>
+        <button className={styles.btn}>Read More</button>
       </div>
     </div>
   );
